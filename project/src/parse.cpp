1d3d7131c23ace4a7cd27ff558a01142c6149ded
druzhinin_daniil <genshpaaack123@gmail.com>
2026-09-21 03:46:18 +0300

    Implement robust event line parser for lesson 1.2

--- project/src/parse.cpp
+++ project/src/parse.cpp
@@ -1,66 +1,120 @@
-#include <parse.h>
+#include "parse.h"
+
+#include <cstddef>
 #include <string>
-#include "event.h"
 
 namespace nano_edr {
-bool ParsePair(const std::string* pair,nano_edr::Field* out){
-    std::size_t pos=(*pair).find('=');
-    if (pos==std::string::npos){
+namespace {
+
+bool IsSpace(char ch) {
+    return ch == ' ' || ch == '\t';
+}
+
+bool ParsePair(const std::string* line, std::size_t* pos, Field* out) {
+    const std::size_t size = line->size();
+    const std::size_t key_start = *pos;
+    std::size_t equal_pos = key_start;
+
+    while (equal_pos < size && (*line)[equal_pos] != '=') {
+        if (IsSpace((*line)[equal_pos])) {
+            return false;
+        }
+        ++equal_pos;
+    }
+
+    if (equal_pos == key_start || equal_pos == size) {
         return false;
     }
-    std::string key=(*pair).substr(0,pos);
-    std::string value=(*pair).substr(pos+1);
-    if (!value.empty() && value[0]=='\"'){
-        if (value[value.size()-1]!='\"'){return false;}
-        value=value.substr(1,value.size()-2);
+
+    const std::string key = line->substr(key_start, equal_pos - key_start);
+    *pos = equal_pos + 1;
+
+    std::string value;
+    if (*pos < size && (*line)[*pos] == '"') {
+        ++(*pos);
+        const std::size_t value_start = *pos;
+        while (*pos < size && (*line)[*pos] != '"') {
+            ++(*pos);
+        }
+
+        if (*pos == size) {
+            return false;
+        }
+
+        value = line->substr(value_start, *pos - value_start);
+        ++(*pos);
+
+        if (*pos < size && !IsSpace((*line)[*pos])) {
+            return false;
+        }
+    } else {
+        const std::size_t value_start = *pos;
+        while (*pos < size && !IsSpace((*line)[*pos])) {
+            ++(*pos);
+        }
+        value = line->substr(value_start, *pos - value_start);
     }
-    if (key==""){
-        return false;
-    }
-    *out={key,value};
+
+    out->key = key;
+    out->value = value;
     return true;
 }
-bool IsBlankOrComment(const std::string* line){
-    if ((*line).empty()){return true;}
-    std::size_t pos=0;
-    while (pos<(*line).size() && ((*line)[pos]==' ' || (*line)[pos]=='\t')){pos++;}
-    if ((*line)[pos]=='#' || (*line)[pos]==';'){return true;}else{return false;}
-}
-bool ParseEventLine(const std::string* line,nano_edr::Event* out){
-    
-    if (IsBlankOrComment(line)){return false;}
-    bool has_ts=false;
-    bool has_type=false;
-    std::size_t pos=0;
-    int ts_count=0;
-    while (pos<(*line).size()) {  
-        while (pos<(*line).size()&& ((*line)[pos]==' ' || (*line)[pos]=='\t')){pos++;}
-        if (pos>=(*line).size()){break;}
-        std::size_t end=pos;
-        bool quotes=false;
 
-        while (end<(*line).size()){
-            if ((*line)[end]=='\"'){quotes=!quotes;}
-            if (!quotes && ((*line)[end]==' ' || (*line)[end]=='\t')){break;}
-            
-            end++;
-        }
-        
-        std::string part_of_line=(*line).substr(pos,end-pos);
-        
-        nano_edr::Field field;
-        if (!ParsePair(&part_of_line,&field)){return false;}
-        if (field.key=="ts"){
-            ++ts_count;
-            if (ts_count>1){out->fields.push_back(field);}
-            else{out->ts=field.value;has_ts=true;}
-        }else if (field.key=="type"){out->type=field.value;has_type=true;}
-        else if (field.key=="pid"){out->pid=field.value;}
-        else{out->fields.push_back(field);}
-        pos=end;
+}  // namespace
+
+bool IsBlankOrComment(const std::string* line) {
+    if (line == nullptr) {
+        return false;
     }
+
+    std::size_t pos = 0;
+    while (pos < line->size() && IsSpace((*line)[pos])) {
+        ++pos;
+    }
+
+    return pos == line->size() || (*line)[pos] == '#' || (*line)[pos] == ';';
+}
+
+bool ParseEventLine(const std::string* line, Event* out) {
+    if (line == nullptr || out == nullptr || IsBlankOrComment(line)) {
+        return false;
+    }
+
+    *out = Event{};
+
+    bool has_ts = false;
+    bool has_type = false;
+    bool has_pid = false;
+    std::size_t pos = 0;
+
+    while (pos < line->size()) {
+        while (pos < line->size() && IsSpace((*line)[pos])) {
+            ++pos;
+        }
+        if (pos == line->size()) {
+            break;
+        }
+
+        Field field;
+        if (!ParsePair(line, &pos, &field)) {
+            return false;
+        }
+
+        if (field.key == "ts" && !has_ts) {
+            out->ts = field.value;
+            has_ts = true;
+        } else if (field.key == "type" && !has_type) {
+            out->type = field.value;
+            has_type = true;
+        } else if (field.key == "pid" && !has_pid) {
+            out->pid = field.value;
+            has_pid = true;
+        } else {
+            out->fields.push_back(field);
+        }
+    }
+
     return has_ts && has_type;
-
-
 }
-}
+
+}  // namespace nano_edr
